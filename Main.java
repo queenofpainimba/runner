1072745c550f2cf772b6cf99fac2c805aa2a50ed
Queen of Pain <queenofpainimba@mail.ru>
2013-06-05 02:00:09 +0400

    First commit - empty player

---
+++ Main.java
@@ -0,0 +1,23 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+	
+	public static final char EMPTY = '.';
+	public static final char LADDER = '=';
+	public static final char BRICK = 'H';
+	public static final char GOLD = '*';
+	public static final char REMOVED_BRICK = '-';
+	
+	public static final String NONE = "NONE";
+	public static final String TOP = "TOP";
+	public static final String LEFT = "LEFT";
+	public static final String RIGHT = "RIGHT";
+	public static final String DIG_LEFT = "DIG_LEFT";
+	public static final String DIG_RIGHT = "DIG_RIGHT";
+	
+	public static void main(String[] args) {
+		
+	}
+
+}
